468ea8f521c6880e0319795f83e1b164546e47ef
jaynish-frontrol <jbuddhdev@frontrol.com>
2023-05-18 12:11:15 +0530

    remove ball boader

--- src/components/ball.js
+++ src/components/ball.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default ({ x, y, radius }) => (
-  <image width={45} height={45} href="https://www.pngall.com/wp-content/uploads/1/Bitcoin-PNG-Pic.png" style={{outline: '3px solid green'}} x={x} y={y} r={radius} />
+  <image width={45} height={45} href="https://www.pngall.com/wp-content/uploads/1/Bitcoin-PNG-Pic.png" x={x} y={y} r={radius} />
 )
